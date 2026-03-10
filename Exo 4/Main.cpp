0fd0eecdff16065d127b14a731a758c7757c8b2c
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-03-10 20:26:04 +0100

    add

--- Exo 4/Main.cpp
+++ Exo 4/Main.cpp
@@ -13,12 +13,13 @@ struct Army
 {
     int index = 0;
     int health = 0;
-    SoldierType type;
+    SoldierType type = SoldierType::NORMAL;
 };
 
 int main()
 {
     int taille;
+    std::cout << "Indique la taille de ton armer : ";
     std::cin >> taille;
 
     Army *army = new Army[taille];
@@ -27,9 +28,13 @@ int main()
     {
         army[i].index = i;
         army[i].health = MAX_HEALTH;
-        army[i].type = (i % 2) == 0 ? SoldierType::HEAVY : SoldierType::NORMAL;  
+        army[i].type = (i % 2) == 0 ? SoldierType::HEAVY : SoldierType::NORMAL;
+
+        std::cout << "Index : " << army[i].index << std::endl;
+        std::cout << "Health : " << army[i].health << std::endl;
+        std::cout << "type : " << (army[i].type == SoldierType::NORMAL ? "Normal" : "Heavy") << std::endl;
     }
-    
+        
     delete[] army;
 
     system("pause");
