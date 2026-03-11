5708f5ea1c7379c3469b7f1347d60ccfb989f89e
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-03-11 20:31:03 +0100

    add

--- Game/Core/Init.h
+++
@@ -1,9 +0,0 @@
-#ifndef INIT_H
-#define INIT_H
-
-#include "../Tools/Common.h"
-
-// Initialize the game data and render window
-// @param _renderWindow: Pointer to the render window
-void Init(GameData* _data);
-#endif
