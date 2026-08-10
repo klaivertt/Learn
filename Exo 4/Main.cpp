71efd2fbcc2e99f326d09e566fe8a35a786532a0
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-08-10 18:17:20 +0200

    string to int

--- Exo 4/Main.cpp
+++ Exo 4/Main.cpp
@@ -3,23 +3,41 @@
 
 #include <iostream>
 
-std::string reverseString(const std::string& _str)
+int string_to_number(const std::string& s)
 {
+	bool isNeg = false;
+	size_t start = 0;
 
-	std::string tempStr = "";
-
-	for (size_t i = _str.length(); i > 0; i--)
+	if (s[0] == '-')
 	{
-		tempStr += _str[i - 1];
+		isNeg = true;
 	}
-	return tempStr;
+	int n = 0;
+	if (isNeg)
+	{
+		start = 1;
+	}
+	for (size_t i = start; i < s.length(); i++)
+	{
+		
+		int tempNb = s[i] - '0';
+		std::cout << tempNb << std::endl;
+		n = n * 10 + tempNb;
+	}
+
+	if (isNeg)
+	{
+		n *= -1;
+	}
+
+	return n;
 }
 
 void main(void)
 {
-	std::string world = "world";
+	std::string nb = "493193";
 
-	std::cout << "reversed world : " << reverseString(world) << std::endl;
+	std::cout << "root numb: " << string_to_number(nb) << std::endl;
 
 	system("pause");
 }
