bcabcb9a3f2c6e8df1bd03fc0817e49842f04900
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-08-10 11:27:17 +0200

    Update Main.cpp

--- Exo 4/Main.cpp
+++ Exo 4/Main.cpp
@@ -1,114 +1,25 @@
-#include "Common.h"
-#include "Game.hpp"
+#include <string>
+#include <stdlib.h>
 
-class PasswordManager
+#include <iostream>
+
+std::string reverseString(const std::string& _str)
 {
-private:
-	int attempt = 5;
-	bool isUnlocked = false;
-	std::string secret = "";
-	std::fstream file;
-	std::string path;
-public:
-	PasswordManager(const std::string _path)
+
+	std::string tempStr = "";
+
+	for (size_t i = _str.length(); i > 0; i--)
 	{
-		path = _path;
-	};
-
-	void EnterPassword()
-	{
-		std::string input;
-
-		while (attempt > 0 && !isUnlocked)
-		{
-			std::cout << "Tentatives restantes : " << attempt << "\nSaisie : ";
-			std::getline(std::cin, input);
-
-			if (input == secret)
-			{
-				isUnlocked = true;
-			}
-			else
-			{
-				attempt--;
-			}
-		}
-	}	
-
-	void AskForPassword()
-	{
-		if (OpenFiles(path))
-		{
-			std::string line;
-			if (!std::getline(file, line))
-			{
-				file.close();
-				std::cout << "no password" << std::endl;
-				std::cout << "Enter a password :";
-				WriteInFiles(path);				
-			}
-			else
-			{
-				SavePassword(line);
-				file.close(); 
-			}
-
-			EnterPassword();
-		}
+		tempStr += _str[i - 1];
 	}
+	return tempStr;
+}
 
-	void WriteInFiles(const std::string _path)
-	{
-		std::ofstream file(_path, std::ios::app);
-		std::string input;
-		std::getline(std::cin, input);
-		SavePassword(input);
-		file << input << std::endl;
-		file.close();
-	}
-
-	bool OpenFiles(const std::string _path)
-	{
-		file = std::fstream(_path, std::ios::in);
-
-		if (!file.is_open())
-		{
-			std::cout << "error, file does Exist" << std::endl;
-			return false;
-		}
-		else
-		{
-			return true;
-		}
-	}
-
-	void SavePassword(const std::string _password)
-	{
-		secret = _password;
-	}
-
-	bool GetIsUnlocked() const
-	{
-		return isUnlocked;
-	}
-};
-
-int main()
+void main(void)
 {
-	PasswordManager* passwordManager = new PasswordManager("password.txt");
+	std::string world = "world";
 
-	passwordManager->AskForPassword();
+	std::cout << "reversed world : " << reverseString(world) << std::endl;
 
-	if (passwordManager->GetIsUnlocked())
-	{
-		std::cout << "succesfull acces to code" << std::endl;
-	}
-	else
-	{
-		std::cout << "acces denied" << std::endl;
-	}
-
-	delete passwordManager;
 	system("pause");
-	return EXIT_SUCCESS;
 }
