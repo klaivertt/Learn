6a4766ffe6d1a1b526c7de48fd9a7c2e4d5eb91f
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-08-12 11:40:02 +0200

    dna to rna

--- Exo 4/Main.cpp
+++ Exo 4/Main.cpp
@@ -1,31 +1,38 @@
-#include <string>
-#include <stdlib.h>
-
 #include <iostream>
 
-
-#include <vector>
-#include <string>
-
-std::string smash(const std::vector<std::string>& _words)
+bool isTriangle(int _a, int _b, int _c)
 {
-	std::string sentences = "";
-	for (int i = 0; i < _words.size(); i++)
-	{
-		if (i != 0)
-		{
-			sentences += " ";
-		}
+    if (_a <= 0 || _b <= 0 || _c <= 0)
+    {
+        return false;
+    }
 
-		sentences += _words[i];
+	int length[3] = { _a, _b, _c };
+
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (length[i] < length[j])
+			{
+				std::swap(length[j],length[i]);
+			}
+		}
 	}
-	return sentences;
+
+	std::cout << length[0] << " + " << length[1] << " = " << length[3] << std::endl;
+	if ((length[0] + length[1]) >= length[3])
+	{
+		return true;
+	}
+	return false;
 }
+
 void main(void)
 {
-	int nb = 0;
 
-	std::cout << "root numb: " << smash({ "hello", "amazing", "world" }) << std::endl;
+	std::cout << "root numb: " << isTriangle(1,1,2) << std::endl;
 
 	system("pause");
 }
