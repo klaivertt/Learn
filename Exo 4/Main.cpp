fcc039f68bca61707ce1daf19f1c41064c476070
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-08-13 17:28:41 +0200

    Update Main.cpp

--- Exo 4/Main.cpp
+++ Exo 4/Main.cpp
@@ -25,10 +25,69 @@ unsigned int number(const std::vector<std::pair<int, int>>& busStops) {
     return passenger >= 0 ? passenger : 0;
 }
 
+static std::string longest(const std::string& s1, const std::string& s2)
+{
+    std::string str = "";
+    str = s1 + s2;
+    for (int i = 0; i < str.size(); i++)
+    {
+        for (int j = 0; j < str.size(); j++)
+        {
+            if (str[i] == str[j])
+            {
+                str.erase(i);
+            }
+            else if (str[i] < str[j])
+            {
+            std::swap(str[j], str[i]);
+            }
+        }
+    }
+    return str;
+}
+
+std::string sliceString(std::string str)
+{
+    if (str.size() < 2)
+    {
+        return "";
+    }
+
+    str.erase(str.front(),1);
+    str.erase(str.back(),1);
+}
+
+bool XO(const std::string& str)
+{
+    int xCount = 0;
+    int oCount = 0;
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] == 'x' || str[i] == 'X')
+        {
+            xCount++;
+        }
+        if (str[i] == 'o' || str[i] == 'O')
+        {
+            oCount++;
+        }
+    }
+
+    if (xCount == oCount)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 void main(void)
 {
 
-	std::cout << "root numb: " << DNAtoRNA("GCAT") << std::endl;
+	std::cout << "root numb: " << XO("zpzpzpp") << std::endl;
 
 	system("pause");
 }
