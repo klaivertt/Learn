df510f9a3b1710f95a3d54de42a5ae7833e4f194
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-08-13 19:15:04 +0200

    Update Main.cpp

--- Exo 4/Main.cpp
+++ Exo 4/Main.cpp
@@ -1,93 +1,135 @@
 #include <iostream>
 
 std::string DNAtoRNA(std::string dna) {
-    for (int i = 0; i < dna.length(); i++)
-    {
-        if (dna[i] == 'T')
-        {
-            dna[i] = 'U';
-        }
-    }
+	for (int i = 0; i < dna.length(); i++)
+	{
+		if (dna[i] == 'T')
+		{
+			dna[i] = 'U';
+		}
+	}
 
-    return dna;
+	return dna;
 }
 
 #include <utility>
 #include <vector>
 
 unsigned int number(const std::vector<std::pair<int, int>>& busStops) {
-    int passenger = 0;
-    for (int i = 0; i < busStops.size(); i++)
-    {
-        passenger += busStops[i].first - busStops[i].second;
-    }
+	int passenger = 0;
+	for (int i = 0; i < busStops.size(); i++)
+	{
+		passenger += busStops[i].first - busStops[i].second;
+	}
 
-    return passenger >= 0 ? passenger : 0;
+	return passenger >= 0 ? passenger : 0;
 }
 
 static std::string longest(const std::string& s1, const std::string& s2)
 {
-    std::string str = "";
-    str = s1 + s2;
-    for (int i = 0; i < str.size(); i++)
-    {
-        for (int j = 0; j < str.size(); j++)
-        {
-            if (str[i] == str[j])
-            {
-                str.erase(i);
-            }
-            else if (str[i] < str[j])
-            {
-            std::swap(str[j], str[i]);
-            }
-        }
-    }
-    return str;
+	std::string str = "";
+	str = s1 + s2;
+	for (int i = 0; i < str.size(); i++)
+	{
+		for (int j = 0; j < str.size(); j++)
+		{
+			if (str[i] == str[j])
+			{
+				str.erase(i);
+			}
+			else if (str[i] < str[j])
+			{
+				std::swap(str[j], str[i]);
+			}
+		}
+	}
+	return str;
 }
 
 std::string sliceString(std::string str)
 {
-    if (str.size() < 2)
-    {
-        return "";
-    }
+	if (str.size() < 2)
+	{
+		return "";
+	}
 
-    str.erase(str.front(),1);
-    str.erase(str.back(),1);
+	str.erase(str.front(), 1);
+	str.erase(str.back(), 1);
 }
 
 bool XO(const std::string& str)
 {
-    int xCount = 0;
-    int oCount = 0;
+	int xCount = 0;
+	int oCount = 0;
 
-    for (int i = 0; i < str.size(); i++)
-    {
-        if (str[i] == 'x' || str[i] == 'X')
-        {
-            xCount++;
-        }
-        if (str[i] == 'o' || str[i] == 'O')
-        {
-            oCount++;
-        }
-    }
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == 'x' || str[i] == 'X')
+		{
+			xCount++;
+		}
+		if (str[i] == 'o' || str[i] == 'O')
+		{
+			oCount++;
+		}
+	}
 
-    if (xCount == oCount)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+	if (xCount == oCount)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+int GetValue(char& _c)
+{
+	switch (_c)
+	{
+	case 'I':
+		return 1;
+	case 'V':
+		return 5;
+	case 'X':
+		return 10;
+	case 'L':
+		return 50;
+	case 'C':
+		return 100;
+	case 'D':
+		return 500;
+	case 'M':
+		return 1000;
+	}
+}
+
+
+int solution(std::string roman)
+{
+	int count = 0;
+
+	for (int i = 0; i < roman.size(); i++)
+	{
+		if (GetValue(roman[i]) >= GetValue(roman[i + 1]))
+		{
+			count += GetValue(roman[i]);
+		}
+		else
+		{
+			count -= GetValue(roman[i]);
+		}
+	}
+
+	return count;
 }
 
 void main(void)
 {
 
-	std::cout << "root numb: " << XO("zpzpzpp") << std::endl;
+	std::cout << "root numb: " << solution("IV") << std::endl;
 
 	system("pause");
 }
