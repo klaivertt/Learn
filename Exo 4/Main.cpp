bc4aa2bfcfb377935a60e59ac6de5750b1daa110
Klaivert <69586612+klaivertt@users.noreply.github.com>
2026-08-14 20:22:53 +0200

    Update Main.cpp

--- Exo 4/Main.cpp
+++ Exo 4/Main.cpp
@@ -1,160 +1,328 @@
 #include <iostream>
+//
+//std::string DNAtoRNA(std::string dna) {
+//	for (int i = 0; i < dna.length(); i++)
+//	{
+//		if (dna[i] == 'T')
+//		{
+//			dna[i] = 'U';
+//		}
+//	}
+//
+//	return dna;
+//}
+//
+//#include <utility>
+//#include <vector>
+//
+//unsigned int number(const std::vector<std::pair<int, int>>& busStops) {
+//	int passenger = 0;
+//	for (int i = 0; i < busStops.size(); i++)
+//	{
+//		passenger += busStops[i].first - busStops[i].second;
+//	}
+//
+//	return passenger >= 0 ? passenger : 0;
+//}
+//
+//static std::string longest(const std::string& s1, const std::string& s2)
+//{
+//	std::string str = "";
+//	str = s1 + s2;
+//	for (int i = 0; i < str.size(); i++)
+//	{
+//		for (int j = 0; j < str.size(); j++)
+//		{
+//			if (str[i] == str[j])
+//			{
+//				str.erase(i);
+//			}
+//			else if (str[i] < str[j])
+//			{
+//				std::swap(str[j], str[i]);
+//			}
+//		}
+//	}
+//	return str;
+//}
+//
+//std::string sliceString(std::string str)
+//{
+//	if (str.size() < 2)
+//	{
+//		return "";
+//	}
+//
+//	str.erase(str.front(), 1);
+//	str.erase(str.back(), 1);
+//}
+//
+//bool XO(const std::string& str)
+//{
+//	int xCount = 0;
+//	int oCount = 0;
+//
+//	for (int i = 0; i < str.size(); i++)
+//	{
+//		if (str[i] == 'x' || str[i] == 'X')
+//		{
+//			xCount++;
+//		}
+//		if (str[i] == 'o' || str[i] == 'O')
+//		{
+//			oCount++;
+//		}
+//	}
+//
+//	if (xCount == oCount)
+//	{
+//		return true;
+//	}
+//	else
+//	{
+//		return false;
+//	}
+//}
+//
+//#include <regex>
+//
+//int GetValue(char& _c)
+//{
+//	switch (_c)
+//	{
+//	case 'I':
+//		return 1;
+//	case 'V':
+//		return 5;
+//	case 'X':
+//		return 10;
+//	case 'L':
+//		return 50;
+//	case 'C':
+//		return 100;
+//	case 'D':
+//		return 500;
+//	case 'M':
+//		return 1000;
+//	}
+//}
+//
+//
+//int solution(std::string roman)
+//{
+//	int count = 0;
+//
+//	for (int i = 0; i < roman.size(); i++)
+//	{
+//		if (GetValue(roman[i]) >= GetValue(roman[i + 1]))
+//		{
+//			count += GetValue(roman[i]);
+//		}
+//		else
+//		{
+//			count -= GetValue(roman[i]);
+//		}
+//	}
+//
+//	return count;
+//}
+//
+//bool sp_eng(const std::string& _sentence)
+//{
+//	std::string word = "english";
+//	// to start of string to the end of this string // here to rewrite become the start, and convert to lower case
+//	std::transform(_sentence.begin(), _sentence.end(), _sentence.begin(), [](unsigned char c) { return std::tolower(c); });
+//
+//	return _sentence.find(word) != std::string::npos; 
+//}
+//
+//int basicOp(char _op, int _val1, int _val2)
+//{
+//	switch (_op)
+//	{
+//	case '+':
+//		return _val1 + _val2;
+//	case '-':
+//		return _val1 - _val2;
+//	case '*':
+//		return _val1 * _val2;
+//	case '/':
+//		return _val2 != 0 ? _val1 / _val2 : 0;
+//	}
+//}
+//
+//std::vector<int> move_zeroes(const std::vector<int>& input)
+//{
+//	// your code here
+//		std::vector<int> temp = input;
+//		for (int i = 0; i < temp.size(); i++)
+//		{
+//			if (temp[i] == 0)
+//			{
+//				temp.erase(temp.begin() + i);
+//				temp.push_back(0);
+//				i--;
+//			}
+//		}
+//		return temp;
+//}
 
-std::string DNAtoRNA(std::string dna) {
-	for (int i = 0; i < dna.length(); i++)
-	{
-		if (dna[i] == 'T')
-		{
-			dna[i] = 'U';
-		}
-	}
 
-	return dna;
-}
-
-#include <utility>
-#include <vector>
-
-unsigned int number(const std::vector<std::pair<int, int>>& busStops) {
-	int passenger = 0;
-	for (int i = 0; i < busStops.size(); i++)
-	{
-		passenger += busStops[i].first - busStops[i].second;
-	}
-
-	return passenger >= 0 ? passenger : 0;
-}
-
-static std::string longest(const std::string& s1, const std::string& s2)
+#include <string>
+std::string add(const std::string& a, const std::string& b)
 {
-	std::string str = "";
-	str = s1 + s2;
-	for (int i = 0; i < str.size(); i++)
+	std::string lower = "";
+	std::string higher = "";
+	if (a.size() > b.size())
 	{
-		for (int j = 0; j < str.size(); j++)
-		{
-			if (str[i] == str[j])
-			{
-				str.erase(i);
-			}
-			else if (str[i] < str[j])
-			{
-				std::swap(str[j], str[i]);
-			}
-		}
-	}
-	return str;
-}
-
-std::string sliceString(std::string str)
-{
-	if (str.size() < 2)
-	{
-		return "";
-	}
-
-	str.erase(str.front(), 1);
-	str.erase(str.back(), 1);
-}
-
-bool XO(const std::string& str)
-{
-	int xCount = 0;
-	int oCount = 0;
-
-	for (int i = 0; i < str.size(); i++)
-	{
-		if (str[i] == 'x' || str[i] == 'X')
-		{
-			xCount++;
-		}
-		if (str[i] == 'o' || str[i] == 'O')
-		{
-			oCount++;
-		}
-	}
-
-	if (xCount == oCount)
-	{
-		return true;
+		lower = b;
+		higher = a;
 	}
 	else
 	{
-		return false;
+		higher = b;
+		lower = a;
 	}
-}
 
-
-int GetValue(char& _c)
-{
-	switch (_c)
+	std::string str = "";
+	int carry = 0;
+	for (int i = lower.size(); i > 0; i--)
 	{
-	case 'I':
-		return 1;
-	case 'V':
-		return 5;
-	case 'X':
-		return 10;
-	case 'L':
-		return 50;
-	case 'C':
-		return 100;
-	case 'D':
-		return 500;
-	case 'M':
-		return 1000;
-	}
-}
-
-
-int solution(std::string roman)
-{
-	int count = 0;
-
-	for (int i = 0; i < roman.size(); i++)
-	{
-		if (GetValue(roman[i]) >= GetValue(roman[i + 1]))
+		int result = (lower[i - 1] - '0') + (higher[(higher.size() - lower.size()) + i - 1] - '0') + carry;
+		if (result > 9)
 		{
-			count += GetValue(roman[i]);
+			result %= 10;
+			carry = 1;
 		}
 		else
 		{
-			count -= GetValue(roman[i]);
+			carry = 0;
+		}
+
+		str.insert(str.begin(), (result + '0'));
+	}
+
+	for (int i = higher.size() - lower.size(); i > 0; i--)
+	{
+		int result = (higher[i - 1] - '0') + carry;
+		if (result > 9)
+		{
+			result %= 10;
+			carry = 1;
+		}
+		else
+		{
+			carry = 0;
+		}
+		str.insert(str.begin(), (result + '0'));
+	}
+
+	if (carry == 1)
+	{
+		str.insert(str.begin(), (carry + '0'));
+	}
+
+	return str;
+}
+long zeros(long n)
+{
+	long numZero = 0;
+	int i = 0;
+	do
+	{
+		numZero += n % int(pow(5, i));
+		i++;
+	} while (pow(5, i) <= n);
+
+	return numZero;
+}
+
+#include <vector>
+
+std::vector<std::vector<int>> create_spiral(int n)
+{
+	if (n < 1)
+	{
+		return {};
+	}
+
+	std::vector<std::vector<int>> grid(n, std::vector<int>(n, 0));
+
+	int minX = 0;
+	int maxX = n - 1;
+	int minY = 0;
+	int maxY = n - 1;
+
+	int x = 0;
+	int y = 0;
+	int v = 1;
+
+	while (minX <= maxX && minY <= maxY)
+	{
+		for (x = minX; x <= maxX; x++)
+		{
+			grid[minY][x] = v++;
+		}
+
+		minY++;
+
+		for (y = minY; y <= maxY; y++)
+		{
+			grid[y][maxX] = v++;
+		}
+
+		maxX--;
+
+		if (minY <= maxY)
+		{
+			for (x = maxX; x >= minX; x--)
+			{
+				grid[maxY][x] = v++;
+			}
+
+			maxY--;
+		}
+
+		if (minX <= maxX)
+		{
+			for (y = maxY; y >= minY; y--)
+			{
+				grid[y][minX] = v++;
+			}
+
+			minX++;
 		}
 	}
 
-	return count;
+	return grid;
 }
 
+
+#include <iostream>
 #include <algorithm>
-bool sp_eng(const std::string& _sentence)
+#include <vector>
+uint64_t descendingOrder(uint64_t a)
 {
-	std::string word = "english";
-	// to start of string to the end of this string // here to rewrite become the start, and convert to lower case
-	std::transform(_sentence.begin(), _sentence.end(), _sentence.begin(), [](unsigned char c) { return std::tolower(c); });
-
-	return _sentence.find(word) != std::string::npos; 
-}
-
-int basicOp(char _op, int _val1, int _val2)
-{
-	switch (_op)
+	uint64_t sorted = 0;
+	std::vector<int> digit;
+	while (a != 0)
 	{
-	case '+':
-		return _val1 + _val2;
-	case '-':
-		return _val1 - _val2;
-	case '*':
-		return _val1 * _val2;
-	case '/':
-		return _val2 != 0 ? _val1 / _val2 : 0;
+		digit.push_back(a % 10);
+		a /= 10;
 	}
+	std::sort(digit.begin(), digit.end(), std::greater<int>());
+
+	for (size_t i = 0; i < digit.size(); i++)
+	{
+		sorted += digit[i] * 10 * (digit.size() - i);
+	}
+	return sorted;
 }
 
 void main(void)
 {
 
-	std::cout << "root numb: " << solution("IV") << std::endl;
-
+	//std::cout << "root numb: " << add("99", "1") << std::endl;
+	create_spiral(1);
 	system("pause");
 }
